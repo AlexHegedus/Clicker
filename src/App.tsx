ac542172558ab1e3526ba049a052d471f74d6a1a
unknown <hegedus.alex.koppany@gmail.com>
2021-07-19 10:57:37 +0200

    Challenge finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  function increase(){
+    setCount(prev => prev + 1)
+  }
+  function decrase(){
+    setCount(prev => prev - 1)
+  }
+  function reset(){
+    setCount(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = {increase}>+</button>
+      <h2>{count}</h2>
+      <button onClick = {decrase}>-</button>
+      <br/>
+      <button onClick = {reset}>Reset</button>
     </div>
   );
 }
